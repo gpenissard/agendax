5017bd7f583730ec19956f89ea457d2fc72bf6b5
Gpenissard <gpenissard@gmail.com>
2020-07-01 16:54:06 -0400

    Promesses controller et chargement DOM

--- fe/src/App.js
+++ fe/src/App.js
@@ -20,12 +20,9 @@ export default class App {
 	/**
 	 * Méthode principale, appelée après le chargement de la page.
 	 */
-	static main() {
-		console.log("Application chargée");
-		console.log("paramètre vi:", App.settings.vi);
-		console.log("paramètre vb:", App.settings.vb);
-		console.log("paramètre vs:", App.settings.vs);
-		console.log("paramètre vo:", App.settings.vo);
+	static domTime() {
+		console.log("Application chargée: Accès au DOM");
+		this.controller.displayContents();
 	}
 
 	/**
@@ -33,17 +30,22 @@ export default class App {
 	 * @returns {Promise} La promesse résolue après chargement DOM
 	 */
 	static load() {
+		console.log("paramètre vi:", App.settings.vi);
+		console.log("paramètre vb:", App.settings.vb);
+		console.log("paramètre vs:", App.settings.vs);
+		console.log("paramètre vo:", App.settings.vo);
+
 		// Routage: Il faudrait créer un petit objet de routage ici
 		console.log("window.location.pathname:", window.location.pathname);
 		if (true /*window.location.pathname.match(/\/event/gi)*/){
 			this._controller = new EventController(); // Controller
 		}
-		this.controller.process();
-
-		return new Promise(resolve => {
+		// Promesses de chargement
+		let controllerLoad = this.controller.process();
+		let DOMLoaded = new Promise(resolve => {
 			document.addEventListener("DOMContentLoaded", () => {
 				resolve();
-			});
-		});
+		})});
+		return Promise.all([controllerLoad,DOMLoaded]);
 	}
 }
--- fe/src/controllers/EventController.js
+++ fe/src/controllers/EventController.js
@@ -15,23 +15,26 @@ export default class EventController extends Controller {
 	process() {
 		const RE_EVENT = /^\/event\/?$/gi;
 		const RE_EVENT_ID = /^\/event\/(\d+)$/gi;
+		let res = null; // Promesse de chargement du controleur 
 		if (window.location.pathname.match(RE_EVENT)){
-			this.events();
+			res = this.events();
 		} else if (window.location.pathname.match(RE_EVENT_ID)) {
 			let eventId = RE_EVENT_ID.exec(window.location.pathname)[1];
-			this.editEvent(eventId);
+			res = this.editEvent(eventId);
 		} else {
 			// throw new Error("Invalid path in EventController");
-			this.events();
+			res = this.events();
 		}
-		this.displayContents();
+		return res;
 	}
 
 	events() {
 		this._page = new EventsPage();
+		return true; // Sera converti en promesse
 	}	
 
 	editEvent(EventId) {
 		this._page = new EditEventPage(EventId);
+		return true; // Sera converti en promesse
 	}
 }
